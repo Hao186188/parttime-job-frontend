0487b7dc2d47156d8c9c02ee240b42f5eedeab2d
Nhathao <vonhathao1862007@gmail.com>
2025-10-21 11:37:36 +0700

    update something

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,9 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
   build: {
-    outDir: 'dist',
-    sourcemap: true,            // tạo sourcemap để debug nếu cần
-    chunkSizeWarningLimit: 1500 // cảnh báo chunk lớn >1.5MB
-  },
-  server: {
-    open: true
+    outDir: 'dist'
   }
-})
+});
